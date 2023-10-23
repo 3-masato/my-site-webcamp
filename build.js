c63c54e8bbdc4ff9e1cdb75d5a6fd8d4c65ee74a
Masato <144527087+3-masato@users.noreply.github.com>
2023-10-23 10:58:35 +0000

    Fixed build.js

--- build.js
+++ build.js
@@ -1,28 +1,33 @@
 import esbuild from "esbuild";
 import { sassPlugin } from "esbuild-sass-plugin";
 import chokidar from "chokidar";
-import { copyFile, readFile, writeFile } from "node:fs/promises";
+
 import { join, basename, sep } from 'node:path';
+
+import _fs from "fs-extra";
+const { readFile, writeFile, copy } = _fs;
+
+import { sleep } from "./src/lib/sleep.js";
 
 const dev = process.argv.includes("dev");
 
 const outdir = dev ? "dev" : "build";
-const htmlfile = "index.html";
+
+const copyHTML = () => copy(`src/index.html`, `${outdir}/index.html`);
+const copyImages = () => copy(`src/img`, `${outdir}/img`);
 
 /** @type {esbuild.BuildOptions} */
 const config = {
   entryPoints: ["src/style.scss"],
   outdir,
   bundle: true,
+  minify: !dev,
   plugins: [sassPlugin()],
   logLevel: "info"
 };
 
 const ctx = await esbuild.context(config);
-
-const sleep = (delay) => new Promise(res => setTimeout(res, delay));
-
-const copyHTML = () => copyFile(`src/${htmlfile}`, `${outdir}/${htmlfile}`);
+await Promise.all([copyHTML(), copyImages()]);
 
 if (dev) {
   await ctx.watch();
@@ -30,19 +35,21 @@ if (dev) {
     servedir: "dev",
     port: 8080
   });
+
   console.log("[esbuild] start watch");
 
   const watcher = chokidar.watch("./src/index.html");
-  await copyHTML();
+
   watcher.on("change", async () => {
-    await sleep(10);
+    await sleep(10); // If I don't put a little delay before copying, a empty file is loaded. Maybe a Cloud9 spec?
+
     await copyHTML();
     await ctx.rebuild();
     console.log("[chokidar] changed");
   });
+
   console.log("[chokidar] start watch");
 } else {
   await ctx.rebuild();
-  await copyHTML();
   process.exit();
 }
---
+++ src/lib/sleep.js
@@ -0,0 +1,1 @@
+export const sleep = (delay) => new Promise(res => setTimeout(res, delay));
